d41c085e7b89c55909cf731b47b64a3272ffd1ee
davendragpm <61841170+davendragpm@users.noreply.github.com>
2021-05-25 17:17:23 +0530

    Add files via upload

---
+++ exam/test.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Success==Practics</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+		<script type="text/javascript" src="js/test.js"></script>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+			
+			</div>
+			<div id="q">
+				<div id="question_container">
+				
+				</div>
+				<div id="question_status">
+				
+				</div>
+			</div>
+			<div class="control" style="float:right; margin-right:10px;">
+				<button onclick='Q_Reset();'>Reset</button>
+				<button onclick='num++;Q_update();'>Save & Next</button>
+				<button onclick='Result();' style="margin-left:110px;">Submit</button>
+			</div>
+		</div>
+	</body>
+</html>
+<script>
+Q_display();
+</script>
