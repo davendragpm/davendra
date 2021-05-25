1c86b20f033916aeca20a394e296aaa16504895f
davendragpm <61841170+davendragpm@users.noreply.github.com>
2021-05-25 17:16:21 +0530

    Create index.php

---
+++ exam/index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Success==Practics</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+		<script type="text/javascript" src="js/index.js"></script>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+			
+			</div>
+			<div id="question_container">
+				
+			</div>
+			<div class="control" style="padding:20px;float:right; margin-right:350px;">
+				<button onclick='num--;Q_display();'>Previous</button>
+				<button onclick='num++;Q_display();'>Next</button>
+				<button onclick='num++;Q_display();'>Save & Next</button>
+			</div>
+		</div>
+	</body>
+</html>
